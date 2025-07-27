8e158841850bba9fdbc6f799ba2ccea6ac7f0a14
Alaa <alaakhaled9191@gmail.com>
2025-07-27 21:11:00 +0300

    remove action bar

--- app/src/main/java/com/madrid/movio/MainActivity.kt
+++ app/src/main/java/com/madrid/movio/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        actionBar?.hide()
         installSplashScreen()
 
         setContent {
