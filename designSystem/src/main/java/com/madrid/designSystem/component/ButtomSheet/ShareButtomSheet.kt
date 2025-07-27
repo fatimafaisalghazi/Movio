9b31588920f4aee459e4c815c28549776f7e2425
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-27 12:27:42 +0300

    feat: Implement Share Bottom Sheet
    This commit introduces a new Share Bottom Sheet component.
    
    - Added `ShareOptionData` data class to hold icon, label, and click action for each share option.
    - Created a `ShareOption` composable to display individual share options with an icon and label.
    - Implemented `ShareViaBottomSheetContent` composable that displays a title "Share via" and a row of share options ("Copy link", "Facebook", "X").
    - Added a preview for `ShareViaBottomSheetContent`.

---
+++ designSystem/src/main/java/com/madrid/designSystem/component/ButtomSheet/ShareButtomSheet.kt
@@ -0,0 +1,114 @@
+package com.madrid.designSystem.component.ButtomSheet
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.madrid.designSystem.R
+import com.madrid.designSystem.theme.Theme
+
+data class ShareOptionData(
+    val icon: Int,
+    val label: String,
+    val onClick: () -> Unit
+)
+
+@Composable
+fun ShareOption(
+    data: ShareOptionData,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .width(74.dp)
+            .height(71.dp)
+            .clickable(onClick = data.onClick),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(48.dp)
+                .clip(CircleShape)
+                .background(Theme.color.surfaces.surfaceContainer),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = data.icon),
+                contentDescription = data.label,
+                tint = Theme.color.surfaces.onSurface
+            )
+        }
+        Text(
+            text = data.label,
+            style = MaterialTheme.typography.labelMedium,
+            color = Theme.color.surfaces.onSurfaceContainer
+        )
+    }
+}
+
+@Composable
+fun ShareViaBottomSheetContent(
+    modifier: Modifier = Modifier,
+    onShareOptionClick: (String) -> Unit
+) {
+    val shareOptions = listOf(
+        ShareOptionData(
+            R.drawable.outline_link_minimalistic,
+            "Copy link"
+        ) { onShareOptionClick("Copy link") },
+        ShareOptionData(R.drawable.facebook, "Facebook") { onShareOptionClick("Facebook") },
+        ShareOptionData(
+            R.drawable.social_icon,
+            "X"
+        ) { onShareOptionClick("X") }
+    )
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 32.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Share via",
+            style = MaterialTheme.typography.titleMedium,
+            color = Theme.color.surfaces.onSurface
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.Top
+        ) {
+            shareOptions.forEach { option ->
+                ShareOption(data = option)
+            }
+        }
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun kfjd(){
+    ShareViaBottomSheetContent(
+        onShareOptionClick = {}
+    )
+}
