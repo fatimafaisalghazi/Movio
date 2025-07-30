ed894934d17ba1eb6c1de83a057aed40b11633aa
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-07-30 18:47:52 +0300

    Refactor: Remove unused paddingValue from MediaCard
    The `paddingValue` parameter in the `MediaCard` composable was not being used and has been removed. The padding is now handled by the `modifier` parameter.
    
    This change affects the following layouts:
    - `AllMediaLayout.kt`
    - `MoviesLayout.kt`
    - `TvShowsLayout.kt`

--- presentation/src/main/java/com/madrid/presentation/screens/homeScreen/layout/MoviesLayout.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/homeScreen/layout/MoviesLayout.kt
@@ -99,7 +99,6 @@ fun MoviesLayout() {
                 movieImage = media.imageUrl,
                 rate = media.rating,
                 height = 220.dp,
-                paddingValue = 8.dp,
                 onClick = {},
                 modifier = Modifier.padding(start = startPaddingValue.dp, end = endPaddingValue.dp)
             )
