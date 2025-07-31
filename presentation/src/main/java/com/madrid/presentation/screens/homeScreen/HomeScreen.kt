b029a83dbaf1cb64a273bfff2f7d941757f301b6
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-07-31 22:37:20 +0300

    feature: add HomeScreenViewModel for HomeScreen and update CategoriesLayout with the required UI

--- presentation/src/main/java/com/madrid/presentation/screens/homeScreen/HomeScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/homeScreen/HomeScreen.kt
@@ -5,34 +5,57 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.madrid.designSystem.component.HeaderSectionBar
 import com.madrid.designSystem.theme.Theme
 import com.madrid.presentation.R
 import com.madrid.presentation.component.header.HomeAppBar
-import com.madrid.presentation.screens.homeScreen.component.TrendingLayout
 import com.madrid.presentation.screens.homeScreen.layout.AllMediaLayout
 import com.madrid.presentation.screens.homeScreen.layout.CategoriesLayout
 import com.madrid.presentation.screens.homeScreen.layout.MoviesLayout
 import com.madrid.presentation.screens.homeScreen.layout.TvShowsLayout
+import com.madrid.presentation.viewModel.homeViewModel.HomeInteractionListener
+import com.madrid.presentation.viewModel.homeViewModel.HomeScreenEffect
+import com.madrid.presentation.viewModel.homeViewModel.HomeScreenState
+import com.madrid.presentation.viewModel.homeViewModel.HomeViewModel
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun HomeScreen() {
-    HomeScreenContent()
+fun HomeScreen(
+    homeViewModel: HomeViewModel = koinViewModel()
+) {
+    val state by homeViewModel.state.collectAsState()
+
+    LaunchedEffect(Unit) {
+        homeViewModel.effect.collect { effect ->
+            when (effect) {
+                is HomeScreenEffect.NavigateToMediaDetails -> {
+                    //TODO()
+                }
+
+                is HomeScreenEffect.NavigateToProfile -> TODO()
+            }
+        }
+    }
+    HomeScreenContent(state = state, homeViewModel)
 }
 
+
 @Composable
-fun HomeScreenContent(){
+fun HomeScreenContent(
+    state: HomeScreenState,
+    interactionListener: HomeInteractionListener
+) {
     var selectedTabIndex by remember { mutableIntStateOf(0) }
 
     Column(
@@ -56,19 +79,41 @@ fun HomeScreenContent(){
             },
             modifier = Modifier.padding(horizontal = 16.dp)
         )
-        LayoutContent(HomeTab.entries[selectedTabIndex])
-
+        LayoutContent(
+            HomeTab.entries[selectedTabIndex],
+            state,
+            interactionListener
+        )
     }
 }
 
 
 @Composable
-private fun LayoutContent(selectedTab: HomeTab){
+private fun LayoutContent(
+    selectedTab: HomeTab,
+    state: HomeScreenState,
+    interactionListener: HomeInteractionListener
+) {
+    Log.d(
+        "HomeScreen: ",
+        "${state.categoryTabUiState.media.collectAsLazyPagingItems().itemCount}"
+    )
     when (selectedTab) {
         HomeTab.ALL -> AllMediaLayout()
         HomeTab.MOVIES -> MoviesLayout()
         HomeTab.TV_SHOWS -> TvShowsLayout()
-        HomeTab.CATEGORIES -> CategoriesLayout()
+        HomeTab.CATEGORIES -> CategoriesLayout(
+            categories = state.categoryTabUiState.categories,
+            selectedCategory = state.categoryTabUiState.selectedCategory,
+            onCategorySelected = { category -> interactionListener.onSelectCategory(category) },
+            mediaItems = state.categoryTabUiState.media.collectAsLazyPagingItems(),
+            sortingType = state.categoryTabUiState.sortingType,
+            onSortingTypeSelected = { sortingType ->
+                interactionListener.onSelectSortingType(
+                    sortingType
+                )
+            },
+        )
     }
 }
 
