0398fc6e2a86bccfe56bad91badb42d733946cd0
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-07-31 16:56:13 +0300

    refactor: move RecommendedSeriesResponse to dto package

--- data/src/main/java/com/madrid/data/dataSource/remote/RemoteDataSourceImpl.kt
+++ data/src/main/java/com/madrid/data/dataSource/remote/RemoteDataSourceImpl.kt
@@ -21,7 +21,7 @@ import com.madrid.data.dataSource.remote.response.movie.NowPlayingMovieResponse
 import com.madrid.data.dataSource.remote.response.movie.UpcomingMoviesResponse
 import com.madrid.data.dataSource.remote.response.series.AiringTodayTvShowsResponse
 import com.madrid.data.dataSource.remote.response.series.OnAirTvShowsResponse
-import com.madrid.data.dataSource.remote.response.series.RecommendedSeriesResponse
+import com.madrid.data.dataSource.remote.dto.series.RecommendedSeriesResponse
 import com.madrid.data.dataSource.remote.response.series.TopRatedSeriesResponse
 import com.madrid.data.repositories.remote.RemoteDataSource
 
