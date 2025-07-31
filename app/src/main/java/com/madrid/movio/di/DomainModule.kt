c55ef1b2023f59776ad8ab3dba11ce8b1b0d4466
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-07-31 16:57:40 +0300

    refactor: add use case declarations to DomainModule

--- app/src/main/java/com/madrid/movio/di/DomainModule.kt
+++ app/src/main/java/com/madrid/movio/di/DomainModule.kt
@@ -2,12 +2,18 @@ package com.madrid.movio.di
 
 import com.madrid.domain.usecase.artist.GetArtistDetailsUseCase
 import com.madrid.domain.usecase.artist.GetArtistMoviesUseCase
+import com.madrid.domain.usecase.movie.FilterMoviesByCategoryUseCase
 import com.madrid.domain.usecase.movie.GetMovieDetailsUseCase
+import com.madrid.domain.usecase.movie.GetMovieGenresUseCase
 import com.madrid.domain.usecase.movie.GetMovieReviewsUseCase
 import com.madrid.domain.usecase.movie.GetMovieTopCastUseCase
 import com.madrid.domain.usecase.movie.GetMovieTrailersUseCase
+import com.madrid.domain.usecase.movie.GetMoviesByGenreIdUseCase
 import com.madrid.domain.usecase.movie.GetMoviesByGenresUseCase
+import com.madrid.domain.usecase.movie.GetNowPlayingMovieUseCase
 import com.madrid.domain.usecase.movie.GetSimilarMoviesUseCase
+import com.madrid.domain.usecase.movie.GetTrendingMoviesUseCase
+import com.madrid.domain.usecase.movie.GetUpcomingMovieUseCase
 import com.madrid.domain.usecase.search.AddRecentSearchUseCase
 import com.madrid.domain.usecase.search.ClearAllRecentSearchesUseCase
 import com.madrid.domain.usecase.search.GetArtistsByQueryUseCase
@@ -18,20 +24,20 @@ import com.madrid.domain.usecase.search.GetRecentSearchesUseCase
 import com.madrid.domain.usecase.search.GetRecommendedMovieUseCase
 import com.madrid.domain.usecase.search.GetSeriesByQueryUseCase
 import com.madrid.domain.usecase.search.RemoveRecentSearchUseCase
+import com.madrid.domain.usecase.series.FilterSeriesByCategoryUseCase
+import com.madrid.domain.usecase.series.GetAiringTodaySeriesUseCase
 import com.madrid.domain.usecase.series.GetEpisodesForSeasonUseCase
+import com.madrid.domain.usecase.series.GetOnAirSeriesUseCase
+import com.madrid.domain.usecase.series.GetRecommendedSeriesUseCase
+import com.madrid.domain.usecase.series.GetSeriesByGenreIdUseCase
 import com.madrid.domain.usecase.series.GetSeriesByGenresUseCase
 import com.madrid.domain.usecase.series.GetSeriesDetailsUseCase
+import com.madrid.domain.usecase.series.GetSeriesGenresUseCase
 import com.madrid.domain.usecase.series.GetSeriesReviewsUseCase
 import com.madrid.domain.usecase.series.GetSeriesTopCastUseCase
 import com.madrid.domain.usecase.series.GetSeriesTrailersUseCase
-import com.madrid.domain.usecase.movie.GetTrendingMoviesUseCase
 import com.madrid.domain.usecase.series.GetSimilarSeriesUseCase
-import com.madrid.domain.usecase.series.FilterSeriesByCategoryUseCase
-import com.madrid.domain.usecase.series.GetSeriesGenresUseCase
 import com.madrid.domain.usecase.series.GetTopRatedSeriesUseCase
-import com.madrid.domain.usecase.series.GetRecommendedSeriesUseCase
-import com.madrid.domain.usecase.series.GetAiringTodaySeriesUseCase
-import com.madrid.domain.usecase.series.GetOnAirSeriesUseCase
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
@@ -59,8 +65,11 @@ val domainModule = module {
     singleOf(::GetSimilarMoviesUseCase)
     singleOf(::GetMovieTrailersUseCase)
     singleOf(::GetTrendingMoviesUseCase)
-
-
+    singleOf(::FilterMoviesByCategoryUseCase)
+    singleOf(::GetMovieGenresUseCase)
+    singleOf(::GetMoviesByGenreIdUseCase)
+    singleOf(::GetNowPlayingMovieUseCase)
+    singleOf(::GetUpcomingMovieUseCase)
     // series
     singleOf(::GetSeriesByGenresUseCase)
     singleOf(::GetEpisodesForSeasonUseCase)
@@ -70,10 +79,10 @@ val domainModule = module {
     singleOf(::GetSeriesTrailersUseCase)
     singleOf(::GetSimilarSeriesUseCase)
     singleOf(::FilterSeriesByCategoryUseCase)
-    singleOf(::GetSeriesGenresUseCase)
-    singleOf(::GetTopRatedSeriesUseCase)
-    singleOf(::GetRecommendedSeriesUseCase)
     singleOf(::GetAiringTodaySeriesUseCase)
     singleOf(::GetOnAirSeriesUseCase)
-
+    singleOf(::GetRecommendedSeriesUseCase)
+    singleOf(::GetSeriesByGenreIdUseCase)
+    singleOf(::GetSeriesGenresUseCase)
+    singleOf(::GetTopRatedSeriesUseCase)
 }
