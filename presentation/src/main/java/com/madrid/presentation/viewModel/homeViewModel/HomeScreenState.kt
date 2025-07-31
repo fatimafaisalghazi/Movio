bc524e480ba783efea2227b864bf0f424f14c78e
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-07-31 16:59:00 +0300

    feature: add HomeScreenState

---
+++ presentation/src/main/java/com/madrid/presentation/viewModel/homeViewModel/HomeScreenState.kt
@@ -0,0 +1,61 @@
+package com.madrid.presentation.viewModel.homeViewModel
+
+import androidx.paging.PagingData
+import com.madrid.presentation.viewModel.shared.MediaUiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+data class HomeScreenState(
+    val isLoading: Boolean = false,
+    val errorMessage: String = "",
+    val allTabUiState: AllTabUiState = AllTabUiState(),
+    val movieTabUiState: MovieTabUiState = MovieTabUiState(),
+    val tvShowTabUiState: TvShowTabUiState = TvShowTabUiState(),
+    val categoryTabUiState: CategoryTabUiState = CategoryTabUiState(),
+)
+
+data class AllTabUiState(
+    val topRated: SectionUiState = SectionUiState(),
+    val trending: SectionUiState = SectionUiState(),
+    val freeToWatch: SectionUiState = SectionUiState(),
+    val upcoming: SectionUiState = SectionUiState(),
+    val moreRecommended: SectionUiState = SectionUiState(),
+)
+
+data class MovieTabUiState(
+    val topRated: SectionUiState = SectionUiState(),
+    val nowPlaying: SectionUiState = SectionUiState(),
+    val upcoming: SectionUiState = SectionUiState(),
+    val moreRecommended: SectionUiState = SectionUiState(),
+)
+
+data class TvShowTabUiState(
+    val topRated: SectionUiState = SectionUiState(),
+    val airingToday: SectionUiState = SectionUiState(),
+    val onTv: SectionUiState = SectionUiState(),
+    val moreRecommended: SectionUiState = SectionUiState(),
+)
+
+data class CategoryTabUiState(
+    val categories: List<CategoryUiState> = emptyList(),
+    val selectedCategory: CategoryUiState = CategoryUiState(),
+    val media: Flow<PagingData<MediaUiState>> = emptyFlow(),
+    val sortingType: SortingType = SortingType.ALL,
+)
+
+data class SectionUiState(
+    val isLoading: Boolean = false,
+    val title: String = "",
+    val media: List<MediaUiState> = emptyList()
+)
+
+data class CategoryUiState(
+    val id: Int = 0,
+    val name: String = "",
+)
+
+enum class SortingType(value: String) {
+    ALL("All"),
+    POPULARITY("Popularity"),
+    LATEST("Latest")
+}
