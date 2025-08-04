98ac76eaa17037e64550c7ff75aeee875cfb3941
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-08-05 02:48:14 +0300

    refactor: rename UserRepository to Authentication repository

--- domain/src/main/java/com/madrid/domain/usecase/movie/GetUserRatedMovieUseCase.kt
+++ domain/src/main/java/com/madrid/domain/usecase/movie/GetUserRatedMovieUseCase.kt
@@ -2,17 +2,17 @@ package com.madrid.domain.usecase.movie
 
 
 import com.madrid.domain.entity.Movie
+import com.madrid.domain.repository.AuthenticationRepository
 import com.madrid.domain.repository.MovieRepository
-import com.madrid.domain.repository.UserRepository
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class GetUserRatedMovieUseCase @Inject constructor(
     private val movieRepository: MovieRepository,
-    private val userRepository: UserRepository
+    private val authenticationRepository: AuthenticationRepository
 ) {
     suspend operator fun invoke(): List<RatedMovie> {
-        val sessionId: String = userRepository.getSessionId().first()
+        val sessionId: String = authenticationRepository.getSessionId().first()
         return movieRepository.getUserMovieRate(
             sessionId = sessionId
         )
--- domain/src/main/java/com/madrid/domain/usecase/series/GetUserRatedSeriesUseCase.kt
+++ domain/src/main/java/com/madrid/domain/usecase/series/GetUserRatedSeriesUseCase.kt
@@ -1,17 +1,17 @@
 package com.madrid.domain.usecase.series
 
 import com.madrid.domain.entity.Series
+import com.madrid.domain.repository.AuthenticationRepository
 import com.madrid.domain.repository.SeriesRepository
-import com.madrid.domain.repository.UserRepository
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class GetUserRatedSeriesUseCase @Inject constructor(
     private val seriesRepository: SeriesRepository,
-    private val userRepository: UserRepository
+    private val authenticationRepository: AuthenticationRepository
 ) {
     suspend operator fun invoke(): List<RatedSeries> {
-        val sessionId: String = userRepository.getSessionId().first()
+        val sessionId: String = authenticationRepository.getSessionId().first()
         return seriesRepository.getUserSeriesRate(sessionId)
     }
 
