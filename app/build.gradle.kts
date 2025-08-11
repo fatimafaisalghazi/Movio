e2df241858f22362b567178f696828e02244f22e
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2025-08-11 07:24:28 +0300

    feat: Enable code minification for release build type in Gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
