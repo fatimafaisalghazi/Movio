fda96e6958026817a0758044f8d9fc7817698134
tasneem-hakeem <tasniim.hakiim@gmail.com>
2025-08-13 14:24:37 +0300

    fix: make app bar sticky at the top

--- presentation/src/main/java/com/madrid/presentation/screens/libraryScreen/layout/WatchListDetailsScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/libraryScreen/layout/WatchListDetailsScreen.kt
@@ -1,9 +1,11 @@
 package com.madrid.presentation.screens.libraryScreen.layout
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -49,35 +51,37 @@ private fun WatchListDetailsScreenContent(
     state: WatchListDetailsState,
     interaction: WatchListDetailsInteractionListener
 ) {
-
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp, horizontal = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+            .statusBarsPadding()
     ) {
-
-        item {
-            TopAppBar(
-                text = state.headerTitle,
-                secondIcon = null,
-                thirdIcon = null,
-                onFirstIconClick = interaction::onNavigateBack,
-            )
-        }
-        items(
-            state.watchList.size, key = { index -> state.watchList[index].id }
-        ) { index ->
-            val watchList = state.watchList[index]
-            SwipeToDeleteCard(
-                title = watchList.title,
-                movieRate = watchList.rating,
-                movieCategory = watchList.category.first().name,
-                movieImageUrl = watchList.imageUrl,
-                onDelete = { interaction.onDeleteMovie(watchList.id) },
-                onClick = { interaction.onClickMovieItem(watchList.id) },
-            )
+        TopAppBar(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = state.headerTitle,
+            secondIcon = null,
+            thirdIcon = null,
+            onFirstIconClick = interaction::onNavigateBack,
+        )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp, horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(
+                state.watchList.size, key = { index -> state.watchList[index].id }
+            ) { index ->
+                val watchList = state.watchList[index]
+                SwipeToDeleteCard(
+                    title = watchList.title,
+                    movieRate = watchList.rating,
+                    movieCategory = watchList.category.first().name,
+                    movieImageUrl = watchList.imageUrl,
+                    onDelete = { interaction.onDeleteMovie(watchList.id) },
+                    onClick = { interaction.onClickMovieItem(watchList.id) },
+                )
+            }
         }
     }
 }
