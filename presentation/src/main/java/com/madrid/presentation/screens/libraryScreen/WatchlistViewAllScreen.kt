778be6cdf189f09b539b04d0809207523b6fe98a
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-08-14 01:25:39 +0300

    feat: handle loading ui

--- presentation/src/main/java/com/madrid/presentation/screens/libraryScreen/WatchlistViewAllScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/libraryScreen/WatchlistViewAllScreen.kt
@@ -1,20 +1,41 @@
 package com.madrid.presentation.screens.libraryScreen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -26,12 +47,12 @@ import com.madrid.presentation.component.videoLibrary.VideoLibrary
 import com.madrid.presentation.navigation.Destinations
 import com.madrid.presentation.navigation.LocalNavController
 import com.madrid.presentation.screens.addtolist.CreateListBottomSheet
-import com.madrid.presentation.screens.addtolist.ListBottomSheetMode
 import com.madrid.presentation.screens.addtolist.SuccessNotificationRow
 import com.madrid.presentation.viewModel.libraryViewModel.watchlistViewAll.WatchListViewAllInteractionListener
 import com.madrid.presentation.viewModel.libraryViewModel.watchlistViewAll.WatchlistViewAllEffect
 import com.madrid.presentation.viewModel.libraryViewModel.watchlistViewAll.WatchlistViewAllUiState
 import com.madrid.presentation.viewModel.libraryViewModel.watchlistViewAll.WatchlistViewAllViewModel
+import com.madrid.presentation.R as presentationR
 
 @Composable
 fun WatchlistViewAllScreen(
@@ -57,7 +78,6 @@ fun WatchlistViewAllScreen(
                 }
             }
         }
-
     }
 
     val interactionListener = viewModel as WatchListViewAllInteractionListener
@@ -76,7 +96,7 @@ fun WatchlistViewAllScreenContent(
         modifier = Modifier.statusBarsPadding()
     ) {
         TopAppBar(
-            text = "Watchlist",
+            text = stringResource(presentationR.string.watchlist),
             secondIcon = null,
             thirdIcon = null,
             onFirstIconClick = { interactionListener.onBackButtonClicked() },
@@ -85,30 +105,25 @@ fun WatchlistViewAllScreenContent(
             )
         )
 
-        if (state.watchLists.isEmpty()) {
-            EmptySearchLayout(
-                title = "\uD83C\uDFAC Nothing here yet!",
-                description = "Add movies and TV shows to build your personal watchlist. The perfect binge starts here!",
-                image = R.drawable.empty,
-                imageSize = 180,
-                modifier = Modifier.fillMaxSize()
-            )
-        } else {
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(minSize = 158.dp),
-                horizontalArrangement = Arrangement.spacedBy(12.dp),
-                contentPadding = PaddingValues(16.dp)
-            ) {
-                items(state.watchLists.size) { index ->
-                    val watchlist = state.watchLists[index]
-                    VideoLibrary(
-                        onClick = { interactionListener.onItemClick(watchlist) },
-                        videosNumber = watchlist.numberOfVideos,
-                        title = watchlist.watchListTitle,
-                        posterUrl = watchlist.posterUrl
-                    )
-                }
-            }
+        AnimatedVisibility(
+            visible = state.isLoading,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) { LoadingContent() }
+
+        AnimatedVisibility(
+            visible = state.isLoading.not() && state.watchLists.isEmpty(),
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            EmptyListContent()
+        }
+        AnimatedVisibility(
+            visible = state.isLoading.not() && state.watchLists.isEmpty().not(),
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            WatchListsGrid(interactionListener, state)
         }
     }
     Box(
@@ -141,5 +156,90 @@ fun WatchlistViewAllScreenContent(
         onDismiss = interactionListener::dismissCreateListBottomSheet,
     )
 
+}
 
+@Composable
+private fun LoadingContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 158.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(16.dp),
+            modifier = Modifier
+        ) {
+            items(12) { index ->
+                Box(
+                    modifier = Modifier
+                        .shimmerEffect()
+                        .size(158.dp, 220.dp)
+                        .clip(shape = RoundedCornerShape(8.dp)),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun EmptyListContent() {
+    EmptySearchLayout(
+        title = "\uD83C\uDFAC Nothing here yet!",
+        description = "Add movies and TV shows to build your personal watchlist. The perfect binge starts here!",
+        image = R.drawable.empty,
+        imageSize = 180,
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+@Composable
+private fun WatchListsGrid(
+    interactionListener: WatchListViewAllInteractionListener,
+    state: WatchlistViewAllUiState
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 158.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(state.watchLists.size) { index ->
+            val watchlist = state.watchLists[index]
+            VideoLibrary(
+                onClick = { interactionListener.onItemClick(watchlist) },
+                videosNumber = watchlist.numberOfVideos,
+                title = watchlist.watchListTitle,
+                posterUrl = watchlist.posterUrl
+            )
+        }
+    }
+}
+
+
+private fun Modifier.shimmerEffect(): Modifier = composed {
+    var size by remember { mutableStateOf(IntSize.Zero) }
+    val transition = rememberInfiniteTransition()
+    val startOffsetX by transition.animateFloat(
+        initialValue = -2 * size.width.toFloat(),
+        targetValue = 2 * size.width.toFloat(),
+        animationSpec = infiniteRepeatable(tween(1000)),
+    )
+
+    background(
+        brush = Brush.linearGradient(
+            colors = listOf(
+                Color(0xFF2A2445),
+                Color(0xFF3D3660),
+                Color(0xFF2A2445),
+            ),
+            start = Offset(startOffsetX, 0f),
+            end = Offset(startOffsetX + size.width.toFloat(), size.height.toFloat())
+        )
+    )
+        .onGloballyPositioned {
+            size = it.size
+        }
 }
