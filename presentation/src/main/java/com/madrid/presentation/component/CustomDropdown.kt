2d571b289184fa8cb8dda44529479ea0271f43ee
fatima faisal ghazi <fatima.faisal.ghazi@gmail.com>
2025-08-14 16:21:28 +0300

    refactor : make the DropdownMenu not appear if the items bigger than 1

--- presentation/src/main/java/com/madrid/presentation/component/CustomDropdown.kt
+++ presentation/src/main/java/com/madrid/presentation/component/CustomDropdown.kt
@@ -4,28 +4,23 @@ package com.madrid.presentation.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExposedDropdownMenuBox
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import com.madrid.designSystem.component.MovioIcon
 import com.madrid.designSystem.component.MovioText
 import com.madrid.designSystem.theme.MovioTheme
@@ -42,15 +37,21 @@ fun <T> CustomDropdown(
     modifier: Modifier = Modifier,
     dropdownWidth: Dp = 120.dp
 ) {
+    var isVisibleContextItem by remember { mutableStateOf(false) }
+    var pressOffset by remember { mutableStateOf(DpOffset.Zero) }
+    var density = LocalDensity.current
+    var itemHeight by remember { mutableStateOf(0.dp) }
     var expanded by remember { mutableStateOf(false) }
+
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = { expanded = it },
         modifier = modifier
     ) {
         Box(
-            modifier = Modifier
-                .size(width = dropdownWidth, height = 32.dp)
+            modifier = modifier
+                .width(dropdownWidth)
+                .onSizeChanged { itemHeight = with(density) { it.height.toDp() } }
                 .background(
                     color = Theme.color.surfaces.surfaceContainer,
                     shape = RoundedCornerShape(32.dp)
@@ -65,7 +66,17 @@ fun <T> CustomDropdown(
             contentAlignment = Alignment.Center
         ) {
             Row(
-                Modifier.padding(horizontal = 12.dp, vertical = 8.dp),
+                Modifier
+                    .padding(horizontal = 12.dp, vertical = 8.dp)
+                    .fillMaxWidth()
+                    .pointerInput(true) {
+                        detectTapGestures(
+                            onLongPress = {
+                                isVisibleContextItem = true
+                                pressOffset = DpOffset(it.x.toDp(), it.y.toDp())
+                            },
+                        )
+                    },
                 horizontalArrangement = Arrangement.spacedBy(4.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -80,40 +91,45 @@ fun <T> CustomDropdown(
                     tint = Theme.color.surfaces.onSurfaceVariant
                 )
             }
-            ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-                modifier = Modifier
-                    .width(dropdownWidth)
-                    .background(
-                        color = Theme.color.surfaces.surface,
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .border(
-                        width = 1.dp,
-                        color = Theme.color.surfaces.onSurfaceAt2,
-                        shape = RoundedCornerShape(8.dp)
-                    )
-                    .padding(8.dp)
-            ) {
-                items.forEach { item ->
-                    DropdownMenuItem(
-                        onClick = {
-                            onItemSelected(item)
-                            expanded = false
-                        }
-                    ) {
-                        MovioText(
-                            text = labelSelector(item),
-                            color = Theme.color.surfaces.onSurfaceContainer,
-                            textStyle = Theme.textStyle.label.smallRegular14
+            if (items.size != 1) {
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                    offset = pressOffset.copy(
+                        y = pressOffset.y - itemHeight * 2
+                    ),
+                    modifier = Modifier
+                        .width(dropdownWidth)
+                        .background(
+                            color = Theme.color.surfaces.surface,
+                            shape = RoundedCornerShape(8.dp)
                         )
+                        .border(
+                            width = 1.dp,
+                            color = Theme.color.surfaces.onSurfaceAt2,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                ) {
+                    items.forEach { item ->
+                        DropdownMenuItem(
+                            onClick = {
+                                onItemSelected(item)
+                                expanded = false
+                            }
+                        ) {
+                            MovioText(
+                                text = labelSelector(item),
+                                color = Theme.color.surfaces.onSurfaceContainer,
+                                textStyle = Theme.textStyle.label.smallRegular14
+                            )
+                        }
                     }
                 }
             }
         }
     }
 }
+
 
 @Preview
 @Composable
