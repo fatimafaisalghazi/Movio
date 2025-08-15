7f6cdbda33e4056821310170bc12c3b41c2caeb6
Baraa Abdulrahman <baraaabdulrahman68@gmail.com>
2025-08-15 17:41:31 +0300

    Refactor: Remove unused rate and date formatting functions
    The `formatRate(String)` and `formatReviewDate(String)` functions in `RateFormatter.kt` were removed as they were no longer in use.

--- presentation/src/main/java/com/madrid/presentation/utils/RateFormatter.kt
+++ presentation/src/main/java/com/madrid/presentation/utils/RateFormatter.kt
@@ -1,10 +1,6 @@
 package com.madrid.presentation.utils
 
-import android.icu.text.SimpleDateFormat
-import kotlinx.datetime.LocalDateTime
 import java.text.DecimalFormat
-import java.time.format.DateTimeFormatter
-import java.util.Locale
 import kotlin.math.floor
 
 object RateFormatter {
@@ -18,26 +14,6 @@ object RateFormatter {
             }
         } catch (e: Exception) {
             rate.toString()
-        }
-    }
-    fun formatRate(rate: String): String {
-        return try {
-            val rateValue = rate.toDouble()
-            formatRate(rateValue)
-        } catch (e: NumberFormatException) {
-            rate
-        }
-    }
-
-    fun formatReviewDate(dateString: String): String {
-        return try {
-            val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH)
-            val outputFormat = SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH)
-            val date = inputFormat.parse(dateString.take(19))
-            outputFormat.format(date)
-
-        } catch (e: Exception) {
-            dateString
         }
     }
 }
