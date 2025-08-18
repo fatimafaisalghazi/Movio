73aeadc1a7a9282f308c2a287b370866b3c0a7cc
Alaa <alaakhaled9191@gmail.com>
2025-08-18 19:50:37 +0300

    refactor border color in basic text field

--- designSystem/src/main/java/com/madrid/designSystem/component/textInputField/BasicTextInputField.kt
+++ designSystem/src/main/java/com/madrid/designSystem/component/textInputField/BasicTextInputField.kt
@@ -42,7 +42,7 @@ fun BasicTextInputField(
     modifier: Modifier = Modifier,
     onClickEndIcon: () -> Unit = { },
     visualTransformation: VisualTransformation = VisualTransformation.None,
-    borderBrushColors: Brush = Theme.color.gradients.borderGradient,
+    borderBrushColors: Brush? = Theme.color.gradients.borderGradient,
     errorBorderBrush: Brush = Theme.color.gradients.errorBorderGradient,
     isError: Boolean = false,
     iconColorInFocus: Color = Theme.color.surfaces.onSurface,
@@ -63,7 +63,7 @@ fun BasicTextInputField(
             shape = RoundedCornerShape(8.dp)
         )
 
-        isFocused || value.isNotEmpty() -> Modifier.border(
+        (isFocused || value.isNotEmpty()) && borderBrushColors != null -> Modifier.border(
             width = 1.dp,
             brush = borderBrushColors,
             shape = RoundedCornerShape(8.dp)
