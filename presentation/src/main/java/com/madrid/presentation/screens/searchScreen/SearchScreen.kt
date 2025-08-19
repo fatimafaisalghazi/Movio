c6c0b7dde0fae229c98be2da26b2deb82b0cfb92
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-08-19 23:33:33 +0300

    manage hide and appear keyboard

--- presentation/src/main/java/com/madrid/presentation/screens/searchScreen/SearchScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/searchScreen/SearchScreen.kt
@@ -27,6 +27,8 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
@@ -199,6 +201,8 @@ fun ContentSearchScreen(
     var showRecentSearch by remember { mutableIntStateOf(0) }
     var isPressSearchIconInKeyboard by remember { mutableIntStateOf(0) }
     var previousSelectedTabIndex by remember { mutableIntStateOf(-1) }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
 
     LaunchedEffect(isDoAction) {
         if(searchQuery.isEmpty() || searchQuery.isBlank() || isPressSearchIconInKeyboard == 1){
@@ -277,6 +281,8 @@ fun ContentSearchScreen(
                                 FilterPagesItem.ARTISTS -> onClickArtist()
                             }
                         }
+                        keyboardController?.hide()
+                        focusManager.clearFocus()
                     }
                 )
             )
