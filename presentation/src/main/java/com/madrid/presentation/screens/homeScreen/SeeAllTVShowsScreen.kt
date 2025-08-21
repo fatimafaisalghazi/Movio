72d44ef1c8bccec85d2340bbb5806ae3e64e7d44
yousef_osama11 <oyousef709@gmail.com>
2025-08-22 01:08:06 +0300

    feature: add light theme drawables

--- presentation/src/main/java/com/madrid/presentation/screens/homeScreen/SeeAllTVShowsScreen.kt
+++ presentation/src/main/java/com/madrid/presentation/screens/homeScreen/SeeAllTVShowsScreen.kt
@@ -168,7 +168,7 @@ fun SeeAllTVShowsScreen(
             DialogWithButtonLayout(
                 title = stringResource(R.string.empty_no_internet_title),
                 description = stringResource(R.string.empty_no_internet_description),
-                image = R.drawable.img_no_internet,
+                image = Theme.drawables.noInternetId,
                 buttonText = stringResource(R.string.try_again),
                 onClick = { viewModel.onTryAgainClick() },
                 imageSize = 170,
@@ -186,7 +186,7 @@ fun SeeAllTVShowsScreen(
             EmptySearchLayout(
                 title = stringResource(R.string.nothing_here_yet),
                 description = stringResource(R.string.category_empty_description),
-                image = R.drawable.img_no_sesrch_found,
+                image = Theme.drawables.notFoundLayoutId,
                 imageSize = 170,
                 modifier = Modifier
                     .fillMaxSize()
